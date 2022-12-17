ca28e42648e8c471551b41a3362a8163497e5203
SkyX [ID FR] <89273191+SkyX-ID-FR@users.noreply.github.com>
2022-12-17 17:55:45 +0100

    Add the first feature.. 📦

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* 
+     ______ __                 __        
+    |      |  |.-----.--.--.--|  |.--.--.
+    |   ---|  ||  _  |  |  |  _  ||  |  |
+    |______|__||_____|_____|_____||___  |
+    Version 1.0 - By SkyX [ID FR] |_____|©
+    2022-2023
+
+*/
+"use strict";
+
+/* 👉 Indicate console.log starting'app : */
+console.clear();
+console.log("%c  Cloudy© is starting ! ✨  ", "border-radius: 10px; padding-top: 3px; padding-bottom: 6px; color: white; font-size: 14px; background-color: green");
+
+/* 🌐 Geolocation of web'client : 🌐 */
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        /* Définir la lattitude et la longitude : */
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+
+        /* Requête avec ces paramêtres à l'API de gep.api.gouv.fr : */
+        fetch(`https://geo.api.gouv.fr/communes?lat=${lat}&lon=${lng}`)
+        .then(response => !response.ok ? Promise.reject() : response.json())
+        .then(json => console.log("Votre ville actuelle 👉 " + json[0].nom))
+        .catch(error => console.log(error))
+    });
+}
